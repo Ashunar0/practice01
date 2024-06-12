45c0455f0d3a4457200f2a06f746df566564120c
Ashunar0 <asahi.gaia1530@gmail.com>
2024-06-12 13:13:33 +0900

    投稿一覧表示・詳細表示まで完了

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <div class='posts'>
+            @foreach ($posts as $post) 
+                <div class='post'>
+                    <p class='id'>{{ $post->id }}</p>
+                    <h2 class='title'>
+                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                    </h2>
+                    <h3 class='body'>{{ $post->body }}</h3>
+                    <p class='created_at'>{{ $post->created_at }}</p>
+                </div>
+            @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
+        </div>
+    </body>
+</html>
